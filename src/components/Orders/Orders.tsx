7d4ad762720926078f4265456278d6a35d55a8f5
Chris Morin <longhumansbiz@gmail.com>
2023-04-02 16:14:00 -0300

    order tables and tabs

--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -1,9 +1,35 @@
 import './orders.css'
 
+const orderCount = [1, 2, 3, 4]
+
+const displayOrders = orderCount.map((order, i) => {
+  console.log(i)
+  if (i % 2 !== 0) {
+
+    return (
+      <div key={i} className='order-row' style={{ background: "#F2F4F7" }}></div>
+    )
+  } else {
+    return (
+      <div key={i} className='order-row'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </div>
+    )
+  }
+})
+
 function Orders() {
   return (
     <div className='orders-container'>
-
+      <div className='recent-orders'></div>
+      <div className='labels'></div>
+      <div>
+        {displayOrders}
+      </div>
     </div>
   )
 }
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,9 +1,22 @@
 import './tabs.css'
 
+function Tab() {
+  return (
+    <div className='tab'>
+      Test
+    </div>
+  )
+}
+
+
 function Tabs() {
   return (
     <div className='tabs-container'>
-
+      <Tab />
+      <Tab />
+      <Tab />
+      <Tab />
+      <Tab />
     </div>
 
   )
