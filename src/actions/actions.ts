78c85906f5225051e6531ee33b2c1d2f67a7f9ef
Chris Morin <longhumansbiz@gmail.com>
2023-04-26 11:51:57 -0300

    actions folder

---
+++ src/actions/actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from '@reduxjs/toolkit'
+
+export const fetchHeaderAction = createAction("FECTH_HEADER_DATA")
+export const fetchOrdersAction = createAction("FECTH_ORDERS_DATA")
